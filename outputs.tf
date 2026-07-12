8fbd0e946dfbd773767a1021895db528ba0991e5
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:03:11 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "maintenance_assignment_dedicated_hosts_id" {
+  description = "Map of id values across all maintenance_assignment_dedicated_hosts, keyed the same as var.maintenance_assignment_dedicated_hosts"
+  value       = { for k, v in azurerm_maintenance_assignment_dedicated_host.maintenance_assignment_dedicated_hosts : k => v.id }
+}
 output "maintenance_assignment_dedicated_hosts_dedicated_host_id" {
   description = "Map of dedicated_host_id values across all maintenance_assignment_dedicated_hosts, keyed the same as var.maintenance_assignment_dedicated_hosts"
   value       = { for k, v in azurerm_maintenance_assignment_dedicated_host.maintenance_assignment_dedicated_hosts : k => v.dedicated_host_id }
