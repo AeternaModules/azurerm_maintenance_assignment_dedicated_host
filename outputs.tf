1181efe07aa46d9576c46b6135ae36559b7f66d7
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:42 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "maintenance_assignment_dedicated_hosts" {
-  description = "All maintenance_assignment_dedicated_host resources"
-  value       = azurerm_maintenance_assignment_dedicated_host.maintenance_assignment_dedicated_hosts
-}
 output "maintenance_assignment_dedicated_hosts_dedicated_host_id" {
-  description = "List of dedicated_host_id values across all maintenance_assignment_dedicated_hosts"
-  value       = [for k, v in azurerm_maintenance_assignment_dedicated_host.maintenance_assignment_dedicated_hosts : v.dedicated_host_id]
+  description = "Map of dedicated_host_id values across all maintenance_assignment_dedicated_hosts, keyed the same as var.maintenance_assignment_dedicated_hosts"
+  value       = { for k, v in azurerm_maintenance_assignment_dedicated_host.maintenance_assignment_dedicated_hosts : k => v.dedicated_host_id }
 }
 output "maintenance_assignment_dedicated_hosts_location" {
-  description = "List of location values across all maintenance_assignment_dedicated_hosts"
-  value       = [for k, v in azurerm_maintenance_assignment_dedicated_host.maintenance_assignment_dedicated_hosts : v.location]
+  description = "Map of location values across all maintenance_assignment_dedicated_hosts, keyed the same as var.maintenance_assignment_dedicated_hosts"
+  value       = { for k, v in azurerm_maintenance_assignment_dedicated_host.maintenance_assignment_dedicated_hosts : k => v.location }
 }
 output "maintenance_assignment_dedicated_hosts_maintenance_configuration_id" {
-  description = "List of maintenance_configuration_id values across all maintenance_assignment_dedicated_hosts"
-  value       = [for k, v in azurerm_maintenance_assignment_dedicated_host.maintenance_assignment_dedicated_hosts : v.maintenance_configuration_id]
+  description = "Map of maintenance_configuration_id values across all maintenance_assignment_dedicated_hosts, keyed the same as var.maintenance_assignment_dedicated_hosts"
+  value       = { for k, v in azurerm_maintenance_assignment_dedicated_host.maintenance_assignment_dedicated_hosts : k => v.maintenance_configuration_id }
 }
 
